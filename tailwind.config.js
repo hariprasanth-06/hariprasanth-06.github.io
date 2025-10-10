4b8becaa3e41b89a60a8e40943219005c5b25f61
hari bangalore <hariprasanth.shanmugam@2kd2.com>
2025-10-11 03:35:04 +0530

    type

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
